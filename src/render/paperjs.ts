930b8142b2c072e0376b292a5406226d4c93c1ad
Izzy Swart <zenerboson@gmail.com>
2019-12-09 10:06:42 -0800

    wip system generation

--- src/render/paperjs.ts
+++ src/render/paperjs.ts
@@ -212,7 +212,10 @@ class PaperMap extends StarMap {
                 });
                 moon_idx += 1;
             })
-            if (planet.moons.length == 1) {
+            if (planet.moons.length == 0) {
+
+            }
+            else if (planet.moons.length = 1) {
                 resource_html += `\nmoon abounds with ${rec2}`;
             } else {
                 resource_html += `\nmoons abound with ${rec2}`
